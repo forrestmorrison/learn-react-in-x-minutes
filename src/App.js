fe6bdc3ca0fff0686a21053ec8a4fa71d6bc208a
forrestmorrison <forrestmorrison@gmail.com>
2022-12-08 14:42:47 -0600

    tested react app, no errors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
 const App = () => {
+
+  const name = 'Forrest';
+  const isNameShowing = true;
+
   return (
     <div className="App">
-      <h1>Hello, React!</h1>
+      <h1>Hello {2 + 2}!</h1>
+      {name ? (
+        <>
+          <h1>{name}</h1>
+        </>
+      ) : (
+        <>
+          <h1>name</h1>
+          <h2>There is no name</h2>
+        </>
+      )}
     </div>
   );
 }
