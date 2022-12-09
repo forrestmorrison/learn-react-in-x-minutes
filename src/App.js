2ae81b976910dd1374b7a1f22afa2ac29eeaa2c6
forrestmorrison <forrestmorrison@gmail.com>
2022-12-09 13:43:45 -0600

    added origin repo for new react app

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import './App.css';
 const App = () => {
 
   const name = 'Forrest';
-  const isNameShowing = true;
+  const isUserLoggedIn = true;
 
   return (
     <div className="App">
       <h1>Hello {2 + 2}!</h1>
-      {name ? (
-        <>
-          <h1>{name}</h1>
-        </>
-      ) : (
-        <>
-          <h1>name</h1>
-          <h2>There is no name</h2>
-        </>
-      )}
     </div>
   );
 }
