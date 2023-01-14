ed5b16869d504cbdd24355ebe3f908aec3fd31d8
forrestmorrison <forrestmorrison@gmail.com>
2023-01-14 13:58:03 -0600

    Events & Prop Drilling, added deleteFeedback function

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,18 @@
+import { FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
 
-const FeedbackItem = ({item}) => {
-    
+const FeedbackItem = ({ item, handleDelete }) => {
+    const handleClick = (id) => {
+        console.log(id)
+    }
+
     return (
         <Card>
             <div className='num-display'>{item.rating}</div>
+            <button onClick={() => handleDelete(item.id)} className='close'>
+                <FaTimes color='purple' />
+            </button>
             <div className='text-display'>{item.text}</div>
         </Card>
     )
