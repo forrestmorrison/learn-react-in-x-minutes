6595f151b4caa9ef9ac2e5da4f6bd5f338f0ec9c
forrestmorrison <forrestmorrison@gmail.com>
2023-01-17 21:28:33 -0600

    Update Feedback Item, Frontend functionality complete

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -48,15 +48,21 @@ export const FeedbackProvider = ({children}) => {
             edit: true
         })
     }
+
+    const updateFeedback = (id, updItem) => {
+        setFeedback(feedback.map((item) => item.id === id ? {
+            ...item, ...updItem
+        } : item))
+    }
     
 
     return <FeedbackContext.Provider value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
         editFeedback,
-        feedbackEdit
-
+        updateFeedback,
     }}>
         {children}
     </FeedbackContext.Provider>
